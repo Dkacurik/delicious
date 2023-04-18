b580aad267ff72942e6f2f54d7f20b7e6333306a
David Kacurik <david.kacurik@gmail.com>
2023-04-18 09:45:56 +0200

    ADD - homepage

--- resources/js/main.js
+++ resources/js/main.js
@@ -61,75 +61,88 @@ const addReservationBtn = document.getElementById('toggleCreateReservationModal'
 const closeReservationBtn = document.getElementById('closeCreateReservationModal')
 const editBtn = document.getElementById('editReservationToggle')
 
-addReservationBtn.addEventListener('click', () => {
-    const createBtn = document.getElementById('addNewReservationToggle')
-    const editBtn = document.getElementById('editReservationToggle')
-    if(!editBtn.classList.contains('hidden')){
-        editBtn.classList.add('hidden')
-    }
-
-    if(createBtn.classList.contains('hidden')){
-        createBtn.classList.remove('hidden')
-    }
-
-    document.getElementById('modalTitle').innerHTML = 'Pridať rezerváciu'
-})
-addReservationBtn.addEventListener('click', toggleCreateReservationModal)
-closeReservationBtn.addEventListener('click', toggleCreateReservationModal)
-editBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const fd = new FormData(document.getElementById('formCreateNewReservation'))
-    const id =  document.getElementById('reservationId').value;
-    const token = document.querySelector('meta[name="csrf-token"]').content;
-    for (var pair of fd.entries()) {
-        console.log(pair[0]+ ', ' + pair[1]);
-    }
-    fetch(`/reservations/${id}`, {
-        method: 'POST',
-        body: fd,
-        headers: {
-            "X-CSRF-TOKEN": token,
+if (addReservationBtn != null){
+    addReservationBtn.addEventListener('click', () => {
+        const createBtn = document.getElementById('addNewReservationToggle')
+        const editBtn = document.getElementById('editReservationToggle')
+        if(!editBtn.classList.contains('hidden')){
+            editBtn.classList.add('hidden')
         }
-    }).then((res) => {
-        if (res.ok) {
-            location.reload();
-        } else {
-            alert('Datum s tymto casom uz bol pouzity')
+
+        if(createBtn.classList.contains('hidden')){
+            createBtn.classList.remove('hidden')
         }
+
+        document.getElementById('modalTitle').innerHTML = 'Pridať rezerváciu'
     })
-})
+    addReservationBtn.addEventListener('click', toggleCreateReservationModal)
+}
+
+if (closeReservationBtn != null){
+    closeReservationBtn.addEventListener('click', toggleCreateReservationModal)
+}
+
+if (editBtn != null){
+    editBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        const fd = new FormData(document.getElementById('formCreateNewReservation'))
+        const id =  document.getElementById('reservationId').value;
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+        for (var pair of fd.entries()) {
+            console.log(pair[0]+ ', ' + pair[1]);
+        }
+        fetch(`/reservations/${id}`, {
+            method: 'POST',
+            body: fd,
+            headers: {
+                "X-CSRF-TOKEN": token,
+            }
+        }).then((res) => {
+            if (res.ok) {
+                location.reload();
+            } else {
+                alert('Datum s tymto casom uz bol pouzity')
+            }
+        })
+    })
+}
 
 const formCreateNewReservation = document.getElementById('formCreateNewReservation')
 
-formCreateNewReservation.addEventListener("submit", (e) => {
-    e.preventDefault();
+if (formCreateNewReservation != null){
+    formCreateNewReservation.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-    const formData = new FormData(formCreateNewReservation);
-    const token = document.querySelector('meta[name="csrf-token"]').content;
-    fetch('/reservations', {
-        method: 'POST',
-        body: formData,
-        headers: {
-            "X-CSRF-TOKEN": token
-        }
-    }).then((res) => {
-        if (res.ok) {
-            location.reload()
-        } else {
-            alert('Datum s tymto casom uz bol pouzity')
-        }
+        const formData = new FormData(formCreateNewReservation);
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+        fetch('/reservations', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                "X-CSRF-TOKEN": token
+            }
+        }).then((res) => {
+            if (res.ok) {
+                location.reload()
+            } else {
+                alert('Datum s tymto casom uz bol pouzity')
+            }
+        })
     })
-})
+}
 
 const deleteReservationButtons = document.getElementsByClassName('remove-reservation')
 
-for (var i = 0; i < deleteReservationButtons.length; i++) {
-    deleteReservationButtons[i].addEventListener('click', removeReservationButton);
+if (deleteReservationButtons != null){
+    for (var i = 0; i < deleteReservationButtons.length; i++) {
+        deleteReservationButtons[i].addEventListener('click', removeReservationButton);
+    }
 }
-
 const editReservationButtons = document.getElementsByClassName('edit-reservation')
 
-for (var i = 0; i < editReservationButtons.length; i++) {
-    editReservationButtons[i].addEventListener('click', editReservationButton);
+if (editReservationButtons != null){
+    for (var i = 0; i < editReservationButtons.length; i++) {
+        editReservationButtons[i].addEventListener('click', editReservationButton);
+    }
 }
 
